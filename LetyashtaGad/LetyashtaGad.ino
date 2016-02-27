89150625140d33b27702ae9372aea78c6d480a63
hrisiz <hrisi.zarkov@abv.bg>
2016-02-28 01:00:28 +0200

    Use classes

--- LetyashtaGad/LetyashtaGad.ino
+++ LetyashtaGad/LetyashtaGad.ino
@@ -52,14 +52,6 @@ Motor motors[4] = {{14,START_POWER},
 int i = 0;
 int input = -1;
 
-int up_drone();
-int down_drone();
-
-int turn_left();
-int turn_right();
-int go_forward();
-int go_back();
-
 void establishContact();
 
 void setup() {
@@ -81,51 +73,48 @@ void setup() {
 void loop(){
   digitalWrite(RED_LED,LOW);
   input = -1;
-  while(input = Serial.read(),  input < 1 && input > 6); // 1 - up all motors 2 - low all motors 3 - left 4 -right 5 - forward 6 - back
+  while((input = Serial.read()) != -1); // 1 - up all motors 2 - low all motors 3 - left 4 -right 5 - forward 6 - back
   
   switch(input){
     case 1:
-      up_drone();
+      for(i = 0; i < 4; i++){
+        motors[i].up_motor_power(STEP);
+      }
       digitalWrite(RED_LED,HIGH);
       delay(300);
       break;
     case 2:
-      down_drone();
+      for(i = 0; i < 4; i++){
+        motors[i].low_motor_power(STEP);
+      }
       digitalWrite(RED_LED,HIGH);
       delay(300);
       break;
-//    case 3:
-//      turn_left();
-//      break;
-//    case 4:
-//      turn_right();
-//      break;
-//    case 5:
-//      go_forward();
-//      break;
-//    case 6:
-//      go_back();
-//      break;
-  }
-}
-
-int up_drone(){
-  for(i = 0; i < 4; i++){
-    motors[i].up_motor_power(STEP);
-  }
-}
-
-int down_drone(){
-  for(i = 0; i < 4; i++){
-    motors[i].low_motor_power(STEP);
+    case 3:
+      motors[2].up_motor_power(STEP);
+      motors[4].up_motor_power(STEP);
+      break;
+    case 4:
+      motors[1].up_motor_power(STEP);
+      motors[3].up_motor_power(STEP);
+      break;
+    case 5:
+      motors[3].up_motor_power(STEP);
+      motors[4].up_motor_power(STEP);
+      break;
+    case 6:
+      motors[1].up_motor_power(STEP);
+      motors[2].up_motor_power(STEP);
+      break;
   }
 }
 
 void establishContact() {
-  while (Serial.available() <= 0) {
+  int check;
+  while (Serial.available() <= 0 && check != 5) {
     digitalWrite(RED_LED,HIGH);
     delay(300);
-    Serial.write(1); // send a capital A
+    check = Serial.write("hello"); 
     digitalWrite(RED_LED,LOW);
     delay(300);
   }
--- LetyashtaGad/LetyashtaGadDistanciono.ino/LetyashtaGadDistanciono.ino.ino
+++ LetyashtaGad/LetyashtaGadDistanciono.ino/LetyashtaGadDistanciono.ino.ino
@@ -1,49 +1,63 @@
 #define CHECK_LIGHT 8
-int inByte = 0; 
-int up = 2;  
-int down = 4; 
-int start = 7; 
-int button[3];
+#define BUTTONS_COUNT 6
+
+class Button{
+  private:
+    int id;
+    int current_value;
+  public:
+    Button(int id){
+      this->id = id;
+    }
+    void initialize(){
+      pinMode(this->id,INPUT_PULLUP);
+    }
+    int get_id(){
+      return this->id;
+    }
+    int get_value(){
+      return this->current_value;
+    }
+    void reset_value(){
+      this->current_value = LOW;
+    }
+    int check_for_push(){
+      this->current_value = digitalRead(id);
+      return this->current_value;
+    }
+};
+
+Button button[BUTTONS_COUNT] = {2,4,7,8,12,13};
+int i = 0;
+
 void setup() {
   pinMode(CHECK_LIGHT,OUTPUT);
+  for(int counter=0; counter < BUTTONS_COUNT; counter++){
+    button[counter].initialize();
+  }
   digitalWrite(CHECK_LIGHT,LOW);
-  pinMode(up, INPUT_PULLUP);
-  pinMode(down, INPUT_PULLUP);
-  pinMode(start, INPUT_PULLUP);
   Serial.begin(9600); 
-  //while(digitalRead(start) == HIGH);
   establishContact();
 }
 
 void loop() {
-  delay(200);
   digitalWrite(CHECK_LIGHT,LOW);
-  while(button[0] = digitalRead(start),button[1] = digitalRead(up),button[2] = digitalRead(down),button[0] == HIGH && button[1] == HIGH && button[2] == HIGH);
-  if(button[0] == LOW){
+  if(button[i].check_for_push() != HIGH){
     if (Serial.available() > 0) {
-      Serial.write(0);
+      Serial.write(i+1);
       digitalWrite(CHECK_LIGHT,HIGH);
+      delay(200);
     }
   }
-  if(button[1] == LOW){
-    if (Serial.available() > 0) {
-      Serial.write(1);
-      digitalWrite(CHECK_LIGHT,HIGH);
-    }
-  }
-  if(button[2] == LOW){
-    if (Serial.available() > 0) {
-      Serial.write(2);
-      digitalWrite(CHECK_LIGHT,HIGH);
-    }
+  button[i].reset_value();
+  if(++i >= BUTTONS_COUNT){
+    i = 0;
   }
 }
 void establishContact() {
-  while (Serial.available() <= 0) {
-    digitalWrite(CHECK_LIGHT,HIGH);
-    delay(300);
-    Serial.write(1);   // send a capital A
-    digitalWrite(CHECK_LIGHT,LOW);
+  int check;
+  while (Serial.available() <= 0 && check != 5) {
+    check = Serial.write("hello");
     delay(300);
   }
 }
