27498f9c51f25741250c91e2176930d7b5fed77f
hrisiz <hrisi.zarkov@abv.bg>
2016-03-25 19:01:08 +0200

    Update

--- LetyashtaGad/LetyashtaGad.ino
+++ LetyashtaGad/LetyashtaGad.ino
@@ -1,10 +1,16 @@
+#include<msp430g2553.h>
 #define STEP 5
+#define UP_DOWN_STEP 10
 #define START_POWER 125
+#define MAX_MOTOR_POWER 255
+#define MIN_MOTOR_POWER 165
 class Motor{
   private:
     int id;
     int current_power;
-    
+    void change_power(int next_power){
+      analogWrite(this->id,next_power);
+    }
   public:
     Motor(int id, int power){
       this->id = id;
@@ -13,13 +19,13 @@ class Motor{
     
     void up_motor_power(int up_with){
       int next_power = this->current_power + up_with;
-      if(next_power > 255){
-         next_power = 255; 
+      if(next_power > MAX_MOTOR_POWER){
+         next_power = MAX_MOTOR_POWER; 
       }
       for(int counter = this->current_power; counter < next_power; counter++){
-        analogWrite( this->id, counter );
+        change_power(counter);
       }
-      analogWrite( this->id, next_power );
+      change_power(next_power);
       this->current_power = next_power;   
     }
     
@@ -29,10 +35,24 @@ class Motor{
          next_power = START_POWER; 
       }
       for(int counter = this->current_power; counter > next_power; counter--){
-        analogWrite( this->id, counter );
+        change_power(counter);
       }
-      analogWrite( this->id, next_power );
+      change_power(next_power);
       this->current_power = next_power;
+    }
+    
+    void set_motor_power(int next_power){
+      if(next_power < this->current_power){
+        for(int counter = current_power; counter > next_power; counter--){
+          change_power(counter);
+        }
+      }else{
+        for(int counter = current_power; counter < next_power; counter++){
+          change_power(counter);
+        }        
+      }
+      change_power(next_power);
+      this->current_power = next_power;   
     }
     
     int check_power(){
@@ -57,7 +77,7 @@ void establishContact();
 void setup() {
   for(i = 0; i < 4; i++){
     pinMode(motors[i].get_id(), OUTPUT); 
-    analogWrite(motors[i].get_id(),START_POWER);
+    motors[i].set_motor_power(START_POWER);
   }
   
   pinMode(RED_LED, OUTPUT);  
@@ -67,54 +87,172 @@ void setup() {
   
   digitalWrite(RED_LED,HIGH);
   
-  while(input = Serial.read(),  input != 1);
+  while(input = Serial.read(),  input != 'R');
+  digitalWrite(RED_LED,LOW);
 }
 
 void loop(){
   digitalWrite(RED_LED,LOW);
   input = -1;
-  while((input = Serial.read()) != -1); // 1 - up all motors 2 - low all motors 3 - left 4 -right 5 - forward 6 - back
-  
+  while((input = Serial.read()) == -1);// 1 - up all motors 2 - low all motors 3 - left 4 -right 5 - forward 6 - back
+  Serial.println(input);
   switch(input){
-    case 1:
+    case '1':
       for(i = 0; i < 4; i++){
-        motors[i].up_motor_power(STEP);
+        if((motors[i].check_power()+UP_DOWN_STEP) > MIN_MOTOR_POWER){
+          motors[i].up_motor_power(UP_DOWN_STEP);
+        }else{
+          motors[i].set_motor_power(MIN_MOTOR_POWER);
+        }
       }
       digitalWrite(RED_LED,HIGH);
       delay(300);
       break;
-    case 2:
+    case '2':
       for(i = 0; i < 4; i++){
-        motors[i].low_motor_power(STEP);
+        if((motors[i].check_power()-UP_DOWN_STEP) > MIN_MOTOR_POWER){
+          motors[i].low_motor_power(UP_DOWN_STEP);
+        }else{
+          motors[i].set_motor_power(MIN_MOTOR_POWER);
+        }
       }
       digitalWrite(RED_LED,HIGH);
       delay(300);
       break;
-    case 3:
-      motors[2].up_motor_power(STEP);
-      motors[4].up_motor_power(STEP);
+    case '3':
+//      if(motors[1].check_power()-STEP <= MIN_MOTOR_POWER || motors[2].check_power()-STEP <= MIN_MOTOR_POWER){
+//        if(motors[1].check_power() < motors[2].check_power()){
+//          motors[1].up_motor_power(STEP);     
+//        }else if(motors[0].check_power() > motors[3].check_power()){
+//          motors[3].up_motor_power(STEP);
+//        }else{
+//          motors[0].up_motor_power(STEP);
+//          motors[3].up_motor_power(STEP);
+//        }
+//      }else{
+//        if(motors[1].check_power() < motors[2].check_power()){
+//          motors[2].low_motor_power(STEP);          
+//        }else if(motors[1].check_power() > motors[2].check_power()){
+//          motors[1].low_motor_power(STEP);
+//        }else{
+//          motors[1].low_motor_power(STEP);
+//          motors[2].low_motor_power(STEP);  
+//        }
+//      }
+      
+      if(motors[1].check_power() > MIN_MOTOR_POWER)
+        motors[1].low_motor_power(STEP);
+      if(motors[2].check_power() > MIN_MOTOR_POWER)
+        motors[2].low_motor_power(STEP); 
+      motors[0].up_motor_power(STEP);
+      motors[3].up_motor_power(STEP);  
+      digitalWrite(RED_LED,HIGH);
+      delay(300);
       break;
-    case 4:
+    case '4':
+//      if(motors[0].check_power()-STEP <= MIN_MOTOR_POWER || motors[3].check_power()-STEP <= MIN_MOTOR_POWER){
+//        if(motors[1].check_power() < motors[2].check_power()){
+//          motors[1].up_motor_power(STEP);     
+//        }else if(motors[1].check_power() > motors[2].check_power()){
+//          motors[2].up_motor_power(STEP);
+//        }else{
+//          motors[1].up_motor_power(STEP);
+//          motors[2].up_motor_power(STEP);
+//        }
+//      }else{
+//        if(motors[0].check_power() < motors[3].check_power()){
+//          motors[3].low_motor_power(STEP);          
+//        }else if(motors[0].check_power() > motors[3].check_power()){
+//          motors[0].low_motor_power(STEP);
+//        }else{
+//          motors[0].low_motor_power(STEP);
+//          motors[3].low_motor_power(STEP);  
+//        }
+//      }
+      if(motors[0].check_power() > MIN_MOTOR_POWER)
+        motors[0].low_motor_power(STEP);
+      if(motors[3].check_power() > MIN_MOTOR_POWER)
+        motors[3].low_motor_power(STEP); 
       motors[1].up_motor_power(STEP);
-      motors[3].up_motor_power(STEP);
+      motors[2].up_motor_power(STEP);  
+      digitalWrite(RED_LED,HIGH);
+      delay(300);
       break;
-    case 5:
-      motors[3].up_motor_power(STEP);
-      motors[4].up_motor_power(STEP);
-      break;
-    case 6:
-      motors[1].up_motor_power(STEP);
+    case '5':
+//      if(motors[0].check_power()-STEP <= MIN_MOTOR_POWER || motors[1].check_power()-STEP <= MIN_MOTOR_POWER){
+//        if(motors[2].check_power() < motors[3].check_power()){
+//          motors[2].up_motor_power(STEP);     
+//        }else if(motors[2].check_power() > motors[3].check_power()){
+//          motors[3].up_motor_power(STEP);
+//        }else{
+//          motors[2].up_motor_power(STEP);
+//          motors[3].up_motor_power(STEP);
+//        }
+//      }else{
+//        if(motors[0].check_power() < motors[1].check_power()){
+//          motors[1].low_motor_power(STEP);          
+//        }else if(motors[0].check_power() > motors[1].check_power()){
+//          motors[0].low_motor_power(STEP);
+//        }else{
+//          motors[0].low_motor_power(STEP);
+//          motors[1].low_motor_power(STEP);  
+//        }
+//      }
+      
+      if(motors[0].check_power() > MIN_MOTOR_POWER)
+        motors[0].low_motor_power(STEP);
+      if(motors[1].check_power() > MIN_MOTOR_POWER)
+        motors[1].low_motor_power(STEP); 
       motors[2].up_motor_power(STEP);
+      motors[3].up_motor_power(STEP);  
+      digitalWrite(RED_LED,HIGH);
+      delay(300);
+      break;
+    case '6':
+//      if(motors[2].check_power()-STEP <= MIN_MOTOR_POWER || motors[3].check_power()-STEP <= MIN_MOTOR_POWER){
+//        if(motors[0].check_power() < motors[1].check_power()){
+//          motors[0].up_motor_power(STEP);     
+//        }else if(motors[0].check_power() > motors[1].check_power()){
+//          motors[1].up_motor_power(STEP);
+//        }else{
+//          motors[0].up_motor_power(STEP);
+//          motors[1].up_motor_power(STEP);
+//        }
+//      }else{
+//        if(motors[2].check_power() < motors[3].check_power()){
+//          motors[3].low_motor_power(STEP);          
+//        }else if(motors[2].check_power() > motors[3].check_power()){
+//          motors[2].low_motor_power(STEP);
+//        }else{
+//          motors[2].low_motor_power(STEP);
+//          motors[3].low_motor_power(STEP);  
+//        }
+//      }
+      if(motors[2].check_power() > MIN_MOTOR_POWER)
+        motors[2].low_motor_power(STEP);
+      if(motors[3].check_power() > MIN_MOTOR_POWER)
+        motors[3].low_motor_power(STEP); 
+      motors[0].up_motor_power(STEP);
+      motors[1].up_motor_power(STEP);  
+      digitalWrite(RED_LED,HIGH);
+      delay(300);
+      break;
+    case '7':
+      for(i = 0; i < 4; i++){
+        motors[i].low_motor_power(UP_DOWN_STEP);
+      }
+      digitalWrite(RED_LED,HIGH);
+      delay(300);
       break;
   }
 }
 
 void establishContact() {
   int check;
-  while (Serial.available() <= 0 && check != 5) {
+  while (Serial.available() <= 0) {
     digitalWrite(RED_LED,HIGH);
     delay(300);
-    check = Serial.write("hello"); 
+    Serial.println("A"); 
     digitalWrite(RED_LED,LOW);
     delay(300);
   }
